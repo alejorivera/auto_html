5f9450e76ff610e2c98769b10cb95c074b763f17
Alejo <alejo@juntostudio.com>
2015-12-10 19:29:14 -0600

    restore to normality

--- lib/auto_html/filters/image.rb
+++ lib/auto_html/filters/image.rb
@@ -1,5 +1,5 @@
 AutoHtml.add_filter(:image) do |text|
-  text.gsub(/(?!cloudfront|src=)https?:\/\/.+?\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
+  text.gsub(/(?<!src=")https?:\/\/.+?\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
     %{<img src="#{match}" class="img-responsive center-block web-img" />}
   end
 end
